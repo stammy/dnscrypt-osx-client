58f045ea8631940be22b61f68bbe87fa98b8f884
Frank Denis <github@pureftpd.org>
2012-07-30 12:44:11 -0700

    Always revert to dhcp right after a network change.

--- CU2/handle-network-change.sh
+++ CU2/handle-network-change.sh
@@ -6,7 +6,6 @@ FALLBACK_FILE="${CONTROL_DIR}/fallback"
 
 [ ! -e "$FALLBACK_FILE" ] && exit 0
 ./check-network-change.sh || exit 0
-./check-hijacking.sh && exit 0
-[ ! -e "$DNSCRYPT_FILE" ] && exit 0
 ./set-dns-to-dhcp.sh
+[ ! -e "$DNSCRYPT_FILE" ] && exit 0
 ./switch-to-dnscrypt.sh
