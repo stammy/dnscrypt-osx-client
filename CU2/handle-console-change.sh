e435ad0cdeeb38b13414d26f1cb1d0a5e1b61afb
Frank Denis <github@pureftpd.org>
2012-07-31 14:17:01 -0700

    Change paths. Scripts can be part of the pref pane.

--- CU2/handle-console-change.sh
+++ CU2/handle-console-change.sh
@@ -4,11 +4,11 @@
 
 eval $(stat -s '/dev/console') || exit 1
 wanted_uid="$st_uid"
-if [ ! -d "$DNSCRYPT_BASE_DIR" ]; then
-  mkdir -p "$DNSCRYPT_BASE_DIR" || exit 1
-  chown -R 0:0 "$DNSCRYPT_BASE_DIR"
+if [ ! -d "$DNSCRYPT_VAR_BASE_DIR" ]; then
+  mkdir -p "$DNSCRYPT_VAR_BASE_DIR" || exit 1
+  chown -R 0:0 "$DNSCRYPT_VAR_BASE_DIR"
 fi
 eval $(stat -s "$CONTROL_DIR") || exit 1
 [ "$st_uid" = "$wanted_uid" ] && exit 0
 chown -R "${wanted_uid}:0" "$CONTROL_DIR"
-find "$DNSCRYPT_BASE_DIR" -type d -exec chmod 755 {} \;
+find "$DNSCRYPT_VAR_BASE_DIR" -type d -exec chmod 755 {} \;
