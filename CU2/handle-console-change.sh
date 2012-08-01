c8f6eae98fdd94eb0f320ed180ded207062b8d47
Frank Denis <github@pureftpd.org>
2012-08-01 10:37:38 -0700

    Add a new script to push tickets

--- CU2/handle-console-change.sh
+++ CU2/handle-console-change.sh
@@ -8,7 +8,7 @@ if [ ! -d "$DNSCRYPT_VAR_BASE_DIR" ]; then
   mkdir -p "$DNSCRYPT_VAR_BASE_DIR" || exit 1
   chown -R 0:0 "$DNSCRYPT_VAR_BASE_DIR"
 fi
+mkdir -p "$TICKETS_DIR" || exit 1
 eval $(stat -s "$CONTROL_DIR") || exit 1
-[ "$st_uid" = "$wanted_uid" ] && exit 0
 chown -R "${wanted_uid}:0" "$CONTROL_DIR"
 find "$DNSCRYPT_VAR_BASE_DIR" -type d -exec chmod 755 {} \;
