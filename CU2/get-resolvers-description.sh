9551962ca2c3260a1ab756c44a44b4e0d37c9224
Frank Denis <github@pureftpd.org>
2012-07-31 19:55:23 -0700

    Improve resolver description

--- CU2/get-resolvers-description.sh
+++ CU2/get-resolvers-description.sh
@@ -1,20 +1,51 @@
 #! /bin/sh
 
-name='Insecure'
+name='None'
+level=0
 while read resolver; do
   case "$resolver" in
     208.67.222.123|208.67.220.123)
-      name='FamilyShield' ;;
+      if [ $level -le 70 ]; then
+        name='FamilyShield'
+        level=70
+      fi
+    ;;
     208.67.*)
-      name='OpenDNS' ;;
+      if [ $level -le 40 ]; then
+        name='OpenDNS'
+        level=40
+      fi
+    ;;
     2620:0:*)
-      name='OpenDNS IPv6' ;;
+      if [ $level -le 50 ]; then
+        name='OpenDNS IPv6'
+        level=50
+      fi
+    ;;
     127.0.0.5*)
-      name='DNSCrypt' ;;
+      if [ $level -le 80 ]; then
+        name='DNSCrypt'
+        level=80
+      fi
+    ;;
     127.0.0.1)
-      name='Localhost' ;;
+      if [ $level -le 20 ]; then
+        name='Localhost'
+        level=20
+      fi
+    ;;
     ::1)
-      name='Localhost IPv6' ;;
+      if [ $level -le 30 ]; then
+        name='Localhost IPv6'
+        level=30
+      fi
+    ;;
+    *)
+      if [ $level -le 10 ]; then
+        name='Insecure'
+        level=10
+      fi
+    ;;
   esac
 done
 echo $name
