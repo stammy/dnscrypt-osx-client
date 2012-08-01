ca334b322b1daf6b39e91cfe94632d1d62a27e9e
Frank Denis <github@pureftpd.org>
2012-08-01 10:46:43 -0700

    Have the scripts handle tickets

--- CU2/handle-control-change.sh
+++ CU2/handle-control-change.sh
@@ -2,16 +2,34 @@
 
 . ./common.inc
 
-lockfile -1 -r 30 -l 60 "$HANDLERS_LOCK_FILE" || exit 1
+PROCESSED_TICKETS_FILE="${STATES_DIR}/processed-tickets"
+
+update() {
+  lockfile -1 -r 30 -l 60 "$HANDLERS_LOCK_FILE" || exit 1
+
+
+  if [ -e "$DNSCRYPT_FILE" ]; then
+    ./switch-to-dnscrypt-if-required.sh
+  else
+    ./stop-dnscrypt-proxy.sh
+    ./switch-to-dhcp-if-required.sh
+  fi
+
+  rm -f "$HANDLERS_LOCK_FILE"
+}
 
 touch "${STATES_DIR}/updating"
-if [ -e "$DNSCRYPT_FILE" ]; then
-  ./switch-to-dnscrypt-if-required.sh
-else
-  ./stop-dnscrypt-proxy.sh
-  ./switch-to-dhcp-if-required.sh
-fi
+
+while :; do
+  find "$TICKETS_DIR" -type f -name 'ticket-*' > "$PROCESSED_TICKETS_FILE"
+  update
+  if [ ! -s "$PROCESSED_TICKETS_FILE" ]; then
+    break
+  fi
+  while read ticket_file; do
+    rm -f "$ticket_file"
+  done < "$PROCESSED_TICKETS_FILE"
+done
+
 rm -f "${STATES_DIR}/updating"
 rm -f "${STATES_DIR}/update-request"
-
-rm -f "$HANDLERS_LOCK_FILE"
