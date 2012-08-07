4b5306351409aa5f0bfe83e3561405f23a83c6e7
Frank Denis <github@pureftpd.org>
2012-08-07 10:36:23 -0700

    Explicitly flush the DNS cache after switching to a new resolver.

--- DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/set-dns-to-dhcp.sh
+++ DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/set-dns-to-dhcp.sh
@@ -11,3 +11,5 @@ exec networksetup -listallnetworkservices 2>/dev/null | \
 fgrep -v '*' | while read x ; do
   networksetup -setdnsservers "$x" "$servers"
 done
+dscacheutil -flushcache 2> /dev/null
+exit 0
--- DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/set-dns.sh
+++ DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/set-dns.sh
@@ -9,3 +9,5 @@ exec networksetup -listallnetworkservices 2>/dev/null | \
 fgrep -v '*' | while read x ; do
   networksetup -setdnsservers "$x" $servers
 done
+dscacheutil -flushcache 2> /dev/null
+exit 0
