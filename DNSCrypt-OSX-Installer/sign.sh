fe647a75c067607d4201db5b56e1e4fe7c295741
Frank Denis <github@pureftpd.org>
2012-08-08 12:14:41 -0700

    0.14

--- DNSCrypt-OSX-Installer/sign.sh
+++ DNSCrypt-OSX-Installer/sign.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-VERSION='0.13'
+VERSION='0.14'
 
 cd build || exit 1
 (
@@ -16,11 +16,11 @@ done
 rm -fr DNSCrypt-OSX.mpkg
 productsign --sign 'Developer ID Application' DNSCrypt.mpkg DNSCrypt-OSX.mpkg
 zip -9 -r "dnscrypt-osx-client-${VERSION}.zip" DNSCrypt-OSX.mpkg
-rm -fr dstroot
+rm -fr dnscrypt-pkg
 rm -f "dnscrypt-osx-client-${VERSION}.dmg"
-mkdir dstroot
-mv DNSCrypt-OSX.mpkg dstroot
-hdiutil create "dnscrypt-osx-client-${VERSION}.dmg" -srcfolder dstroot
+mkdir dnscrypt-pkg
+mv DNSCrypt-OSX.mpkg dnscrypt-pkg
+hdiutil create "dnscrypt-osx-client-${VERSION}.dmg" -srcfolder dnscrypt-pkg
 
-mv dstroot/DNSCrypt-OSX.mpkg .
-rm -fr dstroot
+mv dnscrypt-pkg/DNSCrypt-OSX.mpkg .
+rm -fr dnscrypt-pkg
