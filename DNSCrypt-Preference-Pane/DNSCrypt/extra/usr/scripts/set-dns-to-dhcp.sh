7e3135fe86385897a3a9dcefaa697e36713489a2
Frank Denis <github@pureftpd.org>
2012-09-18 23:07:09 +0200

    Add preliminary support for static DNS settings.

--- DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/set-dns-to-dhcp.sh
+++ DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/set-dns-to-dhcp.sh
@@ -4,12 +4,22 @@
 
 servers='empty'
 
+if [ -r "$STATIC_RESOLVERS_FILE" ]; then
+  servers=''
+  while read server; do
+    case "$server" in
+      [0-9a-fA-F:.]*) servers="${servers} ${server}" ;;
+    esac
+  done < "$STATIC_RESOLVERS_FILE"
+  [ -z "$servers" ] && servers='empty'
+fi
+
 [ -e "$INSECURE_OPENDNS_FILE" ] && servers='208.67.220.220'
 [ -e "$FAMILYSHIELD_FILE" ] && servers='208.67.220.123'
 
 exec networksetup -listallnetworkservices 2>/dev/null | \
 fgrep -v '*' | while read x ; do
-  networksetup -setdnsservers "$x" "$servers"
+  networksetup -setdnsservers "$x" $servers
 done
 dscacheutil -flushcache 2> /dev/null
 killall -HUP mDNSResponder 2> /dev/null
