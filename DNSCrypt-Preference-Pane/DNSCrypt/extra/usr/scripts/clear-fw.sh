e9ef33c6527b2c96f8269be4280542b20b4b0880
Frank Denis <github@pureftpd.org>
2012-09-28 22:42:43 +0200

    Invert the logic, save 2 bytes, yay.

--- DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/clear-fw.sh
+++ DNSCrypt-Preference-Pane/DNSCrypt/extra/usr/scripts/clear-fw.sh
@@ -6,7 +6,7 @@ exec >/dev/null
 exec 2>&1
 
 SOCKETFILTERFW='/usr/libexec/ApplicationFirewall/socketfilterfw'
-[ ! -x "$SOCKETFILTERFW" ] && exit 0
+[ -x "$SOCKETFILTERFW" ] || exit 0
 
 "$SOCKETFILTERFW" --add        /usr/local/sbin/dnscrypt-proxy
 "$SOCKETFILTERFW" --unblockapp /usr/local/sbin/dnscrypt-proxy
