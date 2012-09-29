7bea3fbc639d0db6724aa9288e8a620fdff47b73
Frank Denis <github@pureftpd.org>
2012-09-29 14:55:35 +0200

    Rename DNSCrypt-OSX.mpkg to DNSCrypt.mpkg in order to make Sparkle happy

--- DNSCrypt-OSX-Installer/sign.sh
+++ DNSCrypt-OSX-Installer/sign.sh
@@ -6,21 +6,24 @@ cd build || exit 1
 (
 cd DNSCrypt.mpkg/Contents/Packages || exit 1
 
-for pkg in *pkg; do
+for pkg in *pkg ; do
   rm -fr "x-${pkg}"
   mv "$pkg" "x-${pkg}"
   productsign --sign 'Developer ID Installer' "x-${pkg}" "$pkg"
   rm -fr "x-${pkg}"
 done
 )
-rm -fr DNSCrypt-OSX.mpkg
+rm -fr DNSCrypt-OSX.mpkg DNSCrypt-unsigned.mpkg
 productsign --sign 'Developer ID Application' DNSCrypt.mpkg DNSCrypt-OSX.mpkg
-zip -9 -r "dnscrypt-osx-client-${VERSION}.zip" DNSCrypt-OSX.mpkg
+mv DNSCrypt.mpkg DNSCrypt-unsigned.mpkg
+mv DNSCrypt-OSX.mpkg DNSCrypt.mpkg
+zip -9 -r "dnscrypt-osx-client-${VERSION}.zip" DNSCrypt.mpkg
 rm -fr dnscrypt-pkg
 rm -f "dnscrypt-osx-client-${VERSION}.dmg"
 mkdir dnscrypt-pkg
-mv DNSCrypt-OSX.mpkg dnscrypt-pkg
+mv DNSCrypt.mpkg dnscrypt-pkg
 hdiutil create "dnscrypt-osx-client-${VERSION}.dmg" -srcfolder dnscrypt-pkg
 
-mv dnscrypt-pkg/DNSCrypt-OSX.mpkg .
+mv dnscrypt-pkg/DNSCrypt.mpkg .
 rm -fr dnscrypt-pkg
+rm -fr DNSCrypt-unsigned.mpkg
