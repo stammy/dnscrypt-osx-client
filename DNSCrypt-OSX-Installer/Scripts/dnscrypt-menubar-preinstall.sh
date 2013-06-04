80d0323adb7ae10dd4810608d665d35df4ca4382
Frank Denis <github@pureftpd.org>
2013-06-03 22:28:33 -0700

    Disable the OpenDNS Umbrella services if they happen to be running.

--- DNSCrypt-OSX-Installer/Scripts/dnscrypt-menubar-preinstall.sh
+++ DNSCrypt-OSX-Installer/Scripts/dnscrypt-menubar-preinstall.sh
@@ -2,6 +2,10 @@
 
 exec 2>/dev/null
 
+/bin/launchctl remove com.opendns.osx.RoamingClientMenubar
+/bin/launchctl remove com.opendns.osx.RoamingClientConfigUpdater
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.Roaming*.plist
+
 /bin/launchctl stop com.opendns.osx.DNSCryptMenuBar
 /bin/launchctl remove com.opendns.osx.DNSCryptMenuBar
 
