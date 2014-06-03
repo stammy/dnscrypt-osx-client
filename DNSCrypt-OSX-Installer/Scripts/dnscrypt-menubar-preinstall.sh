12eff619cce4647e8ff08b50606398a8a470e913
Alter Step <alterstep@localhost>
2014-06-02 21:40:59 -0700

    Improve preinstall scripts

--- DNSCrypt-OSX-Installer/Scripts/dnscrypt-menubar-preinstall.sh
+++ DNSCrypt-OSX-Installer/Scripts/dnscrypt-menubar-preinstall.sh
@@ -2,14 +2,24 @@
 
 exec 2>/dev/null
 
+/bin/launchctl stop com.opendns.osx.RoamingClientMenubar
 /bin/launchctl remove com.opendns.osx.RoamingClientMenubar
-/bin/launchctl remove com.opendns.osx.RoamingClientConfigUpdater
-/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.Roaming*.plist
 
-/bin/launchctl remove com.opendns.osx.DNSCryptMenuBar
-/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCryptMenuBar.plist
+/bin/launchctl stop com.opendns.osx.RoamingClientConfigUpdater
+/bin/launchctl remove com.opendns.osx.RoamingClientConfigUpdater
+
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.Roaming*.plist
 
 /bin/launchctl stop com.opendns.osx.DNSCryptMenuBar
 /bin/launchctl remove com.opendns.osx.DNSCryptMenuBar
+
+/bin/launchctl stop com.github.dnscrypt-osxclient.DNSCryptMenuBar
+/bin/launchctl remove com.github.dnscrypt-osxclient.DNSCryptMenuBar
+
+killall 'DNSCrypt-Menubar'
+killall 'DNSCrypt Menubar'
+
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCryptMenuBar.plist
+/bin/rm -f /Library/LaunchDaemons/com.github.dnscrypt-osxclient.DNSCryptMenuBar.plist
 
 exit 0
--- DNSCrypt-OSX-Installer/Scripts/dnscrypt-prefpane-preinstall.sh
+++ DNSCrypt-OSX-Installer/Scripts/dnscrypt-prefpane-preinstall.sh
@@ -2,28 +2,45 @@
 
 exec 2>/dev/null
 
-if [ -x /usr/bin/pkill ]; then
-  /usr/bin/pkill -f '/Applications/System Preferences.app'
-fi
+killall 'System Preferences'
 
 /bin/rm -rf '/Library/Application Support/DNSCrypt Updater'
 
+/bin/launchctl stop com.opendns.osx.DNSCryptConfigUpdater
 /bin/launchctl remove com.opendns.osx.DNSCryptConfigUpdater
 /bin/rm -f '/Library/LaunchDaemons/com.opendns.osx.DNSCryptConfigUpdater.plist'
 
-/bin/launchctl remove com.opendns.osx.DNSCryptAfterboot
-/bin/launchctl remove com.opendns.osx.DNSCryptConsoleChange
-/bin/launchctl remove com.opendns.osx.DNSCryptControlChange
-/bin/launchctl remove com.opendns.osx.DNSCryptNetworkChange
-/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCrypt*.plist
-
+/bin/launchctl stop com.github.dnscrypt-osxclient.DNSCryptConfigUpdater
 /bin/launchctl remove com.github.dnscrypt-osxclient.DNSCryptConfigUpdater
 /bin/rm -f '/Library/LaunchDaemons/com.github.dnscrypt-osxclient.DNSCryptConfigUpdater.plist'
 
+/bin/launchctl stop com.opendns.osx.DNSCryptAfterboot
+/bin/launchctl remove com.opendns.osx.DNSCryptAfterboot
+/bin/launchctl stop com.opendns.osx.DNSCryptConsoleChange
+/bin/launchctl remove com.opendns.osx.DNSCryptConsoleChange
+/bin/launchctl stop com.opendns.osx.DNSCryptControlChange
+/bin/launchctl remove com.opendns.osx.DNSCryptControlChange
+/bin/launchctl stop com.opendns.osx.DNSCryptNetworkChange
+/bin/launchctl remove com.opendns.osx.DNSCryptNetworkChange
+
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCryptAfterboot.plist
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCryptConsoleChange.plist
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCryptControlChange.plist
+/bin/rm -f /Library/LaunchDaemons/com.opendns.osx.DNSCryptNetworkChange.plist
+
+/bin/launchctl stop com.github.dnscrypt-osxclient.DNSCryptAfterboot
 /bin/launchctl remove com.github.dnscrypt-osxclient.DNSCryptAfterboot
+/bin/launchctl stop com.github.dnscrypt-osxclient.DNSCryptConsoleChange
 /bin/launchctl remove com.github.dnscrypt-osxclient.DNSCryptConsoleChange
+/bin/launchctl stop com.github.dnscrypt-osxclient.DNSCryptControlChange
 /bin/launchctl remove com.github.dnscrypt-osxclient.DNSCryptControlChange
+/bin/launchctl stop com.github.dnscrypt-osxclient.DNSCryptNetworkChange
 /bin/launchctl remove com.github.dnscrypt-osxclient.DNSCryptNetworkChange
+
+/bin/rm -f /Library/LaunchDaemons/com.github.dnscrypt-osxclient.DNSCryptAfterboot.plist
+/bin/rm -f /Library/LaunchDaemons/com.github.dnscrypt-osxclient.DNSCryptConsoleChange.plist
+/bin/rm -f /Library/LaunchDaemons/com.github.dnscrypt-osxclient.DNSCryptControlChange.plist
+/bin/rm -f /Library/LaunchDaemons/com.github.dnscrypt-osxclient.DNSCryptNetworkChange.plist
 
 /bin/rm -f /var/run/dnscrypt*.lock
 
